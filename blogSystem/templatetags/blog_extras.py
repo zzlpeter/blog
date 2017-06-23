21ef7194be03e7586c2c7abf3c0ebc6b22afa6f3
zhangzhiliang <zhangzhiliang@cmcm.com>
2017-06-23 14:39:25 +0800

    博客系统雏形

--- blogSystem/templatetags/blog_extras.py
+++ blogSystem/templatetags/blog_extras.py
@@ -5,6 +5,7 @@ from django import template
 register = template.Library()
 
 
+# django model func template parameter
 @register.simple_tag
 def get_post_related_num(func, params):
     return func.get_post_related_num(params)
