aa5c9cd9685764c8fad110bcafe45768e5970a99
zhangzhiliang <zhangzhiliang@cmcm.com>
2017-06-27 13:34:21 +0800

    博客系统雏形

--- blogSystem/templatetags/blog_extras.py
+++ blogSystem/templatetags/blog_extras.py
@@ -1,11 +1,25 @@
 # coding: utf-8
 
 from django import template
+from blogSystem.models import UserExtend
+import logging
 
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 
 # django model func template parameter
 @register.simple_tag
 def get_post_related_num(func, params):
     return func.get_post_related_num(params)
+
+
+# get user's portrait path
+@register.filter
+def get_user_portrait(user):
+    try:
+        obj = UserExtend.objects.get(pk=user.id)
+        return '%s/%s/%s' % ('/static/images', obj.portrait.img_category.name, obj.portrait.src)
+    except Exception, exc:
+        logger.error(exc, exc_info=True)
+        return '/static/images/other/anonymous.jpg'
